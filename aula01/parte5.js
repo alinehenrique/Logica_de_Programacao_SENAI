2a2a47c770373be430b8b010d4535ce8978a6d49
Aline Henrique <152557947+alinehenrique@users.noreply.github.com>
2023-12-11 13:03:33 -0300

    Delete aula01 directory

--- aula01/parte5.js
+++
@@ -1,8 +0,0 @@
-const listaDeNomes = ["Aline", "Pedro", "Maria", "Paulo", "Fulano"];
-
-console.log(listaDeNomes.length); 
-
-for (let indice = 0; indice < listaDeNomes.length; indice++) {
-  const nome = listaDeNomes[indice];
-  console.log(nome);
-}
